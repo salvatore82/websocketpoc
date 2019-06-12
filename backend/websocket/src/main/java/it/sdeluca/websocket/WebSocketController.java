55208b244883eab6162142044a68a273ea1bd3c0
Salvatore De Luca <s.deluca@almaviva.it>
2019-06-12 18:11:20 +0200

    add swagger for clean api

--- backend/websocket/src/main/java/it/sdeluca/websocket/WebSocketController.java
+++ backend/websocket/src/main/java/it/sdeluca/websocket/WebSocketController.java
@@ -1,13 +1,11 @@
 package it.sdeluca.websocket;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
